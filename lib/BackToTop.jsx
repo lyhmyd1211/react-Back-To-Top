462221ad001d41a22ca7e94e631a592eda1a4667
luyingheng <75986899@qq.com>
2020-01-22 14:03:52 +0800

    update to support SSR and custiom text

--- lib/BackToTop.jsx
+++ lib/BackToTop.jsx
@@ -5,50 +5,82 @@ import './BackToTop.css';
 class BackToTop extends Component {
   constructor(props) {
     super(props);
-    this.state={
-      percent:0,
-      current:0,
+    this.state = {
+      percent: 0,
+      current: 0
     };
-    this.animate = this.props.animate||'fade'; /**1、fade(default) 2、rotate 3、none */
-    this.mainStyle = this.props.mainStyle || {
-      
-    };
-    this.percentStyle = this.props.percentStyle||{};
-    this.offsetTop = this.props.offsetTop||0;
-    this.step = this.props.step||50;
-    this.visiblePercent = this.props.visiblePercent > 0 ? this.props.visiblePercent:1;
-    this.isPercent = this.props.isPercent === undefined ? true : this.props.isPercent;
-    window.onscroll = ()=>{
-      this.setState({ percent: getScrollPercent(this.props.offsetTop) });
-      this.setState({current:getScrollTop()});
-    };
-   
+    this.animate =
+      this.props.animate || 'fade'; /**1、fade(default) 2、rotate 3、none */
+    this.mainStyle = this.props.mainStyle || {};
+    this.percentStyle = this.props.percentStyle || {};
+    this.offsetTop = this.props.offsetTop || 0;
+    this.step = this.props.step || 50;
+    this.visiblePercent =
+      this.props.visiblePercent > 0 ? this.props.visiblePercent : 1;
+    this.isPercent =
+      this.props.isPercent === undefined ? true : this.props.isPercent;
   }
-  scrollToTop(){
+  scrollToTop() {
     ScrollToAnimate(this.props.offsetTop, this.props.step, this.state.current);
-  };
-  componentDidMount() { }
-  render() {
-    const{animate} = this.props;
-    let visible = this.state.percent >= this.visiblePercent;
-    let animateHide = "c-animate-hide";
-    let animateShow = "c-animate-show";
-    if (animate!=="fade"&&animate!=="rotate") {
-      if (animate!=='none') {
-        animateHide = "fade-hide";
-        animateShow = "fade-show";
-      }
-    }else{
-      animateHide = animate + "-hide";
-      animateShow = animate + "-show";
+  }
+  componentDidMount() {
+    if (window) {
+      window.onscroll = () => {
+        this.setState({
+          percent: getScrollPercent(this.props.offsetTop),
+          current: getScrollTop()
+        });
+        // this.setState({ current: getScrollTop() });
+      };
     }
-    return <div onClick={() => this.scrollToTop()} className={"base-back-to-up " + (visible ? animateShow : animateHide)}>
+  }
+  render() {
+    const { animate, children } = this.props;
+    let visible = this.state.percent >= this.visiblePercent;
+    let animateHide = 'c-animate-hide';
+    let animateShow = 'c-animate-show';
+    if (animate !== 'fade' && animate !== 'rotate') {
+      if (animate !== 'none') {
+        animateHide = 'fade-hide';
+        animateShow = 'fade-show';
+      }
+    } else {
+      animateHide = animate + '-hide';
+      animateShow = animate + '-show';
+    }
+    console.log('asdasd', this.props, this.props.children);
+    return (
+      <div
+        onClick={() => this.scrollToTop()}
+        className={'base-back-to-up ' + (visible ? animateShow : animateHide)}
+      >
         {/* <Icon type="up-square" className="to-top-icon" /> */}
-        <div className='back-to-up-default' style={this.mainStyle} />
-      <div className={"to-up-percent" + (this.isPercent ? "" :" percent-hide")}   style={ this.percentStyle }>
-          {this.state.percent + "%"}
+        <div className="back-to-up-default" style={this.mainStyle}>
+          {children ? (
+            Array.isArray(children) ? (
+              children.map(child, index => {
+                return <div key={index}>{child}</div>;
+              })
+            ) : (
+              children && <div>{children}</div>
+            )
+          ) : (
+            <div className="back-to-up-default-text">
+              <span>UP</span>
+            </div>
+          )}
+
+          <div
+            className={
+              'to-up-percent' + (this.isPercent ? '' : ' percent-hide')
+            }
+            style={this.percentStyle}
+          >
+            {this.state.percent + '%'}
+          </div>
         </div>
-      </div>;
+      </div>
+    );
   }
 }
 export default BackToTop;
